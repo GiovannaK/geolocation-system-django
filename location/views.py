bf221be71fefc1b2eb8a5fe3ea16b8d597a8fa95
Giovanna Cunha <giovannakelli@gmail.com>
2020-10-08 00:21:06 -0300

    adding TemplateView to render index template

--- location/views.py
+++ location/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from .models import Location
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'location/index.html'
+
 
